5351f11967bee538a0e5dbd56d44bfdbef32742a
WMXPY <wm@mengw.io>
2020-09-01 15:18:31 -0500

    implement verfiy

--- src/jwt.ts
+++ src/jwt.ts
@@ -4,16 +4,50 @@
  * @description JWT
  */
 
+import { SignatureVerifier } from "@sudoo/token";
 import { JWTJoinedHeader } from "./declare";
 
-export const verifySurfacePattern = (token: string): boolean => {
+export const deconstructJWT = (token: string): [string, string, string] => {
 
     const tuple: [string, string, string] = token.split('.') as [string, string, string];
+
+    return tuple;
+};
+
+export const verifySurfaceTokenPattern = (token: string): boolean => {
+
+    const tuple: [string, string, string] = deconstructJWT(token);
     if (tuple.length !== 3) {
         return false;
     }
 
     return true;
+};
+
+export const verifyTokenSignatureByTuple = (tuple: [string, string, string], publicKey: string): boolean => {
+
+    const serializedHeader: string = tuple[0];
+    const serializedBody: string = tuple[1];
+    const signature: string = tuple[2];
+
+    const joinedContent: string = [
+        serializedHeader,
+        serializedBody,
+    ].join('.');
+
+    const signatureVerifier: SignatureVerifier = SignatureVerifier.create(publicKey);
+
+    return signatureVerifier.verify(joinedContent, signature);
+};
+
+export const verifyTokenSignatureByToken = (token: string, publicKey: string): boolean => {
+
+    const tuple: [string, string, string] = deconstructJWT(token);
+    if (tuple.length !== 3) {
+        return false;
+    }
+
+    return verifyTokenSignatureByTuple(tuple, publicKey);
 };
 
 export const fixJWTHeader = <Header extends Record<string, any>>(header: Header): JWTJoinedHeader<Header> => {
--- src/token.ts
+++ src/token.ts
@@ -4,9 +4,36 @@
  * @description Token
  */
 
+import { verifySurfaceTokenPattern } from "./jwt";
+
 export class JWTToken<Header extends Record<string, any> = any, Body extends Record<string, any> = any> {
 
-    public static createWithoutVerify<Header extends Record<string, any> = any, Body extends Record<string, any> = any>(token: string): JWTToken<Header, Body> {
+    public static createWithoutVerify<Header extends Record<string, any> = any, Body extends Record<string, any> = any>(token: string): JWTToken<Header, Body> | null {
+
+        const surfaceVerifyResult: boolean = verifySurfaceTokenPattern(token);
+        if (!surfaceVerifyResult) {
+            return null;
+        }
+
+        return new JWTToken<Header, Body>(token);
+    }
+
+    public static createWithPatternVerify<Header extends Record<string, any> = any, Body extends Record<string, any> = any>(token: string): JWTToken<Header, Body> | null {
+
+        const surfaceVerifyResult: boolean = verifySurfaceTokenPattern(token);
+        if (!surfaceVerifyResult) {
+            throw new Error('[Sudoo-JWT] Invalid JWT Token');
+        }
+
+        return new JWTToken<Header, Body>(token);
+    }
+
+    public static createWithLocalVerify<Header extends Record<string, any> = any, Body extends Record<string, any> = any>(token: string): JWTToken<Header, Body> | null {
+
+        const surfaceVerifyResult: boolean = verifySurfaceTokenPattern(token);
+        if (!surfaceVerifyResult) {
+            throw new Error('[Sudoo-JWT] Invalid JWT Token');
+        }
 
         return new JWTToken<Header, Body>(token);
     }
