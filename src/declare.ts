13c035f2673bffc7786b820162f578dac3defdc1
WMXPY <wm@mengw.io>
2020-09-01 18:23:07 -0500

    add create ignite

--- src/declare.ts
+++ src/declare.ts
@@ -10,6 +10,15 @@ export type JWTFixedHeader = {
     readonly typ: 'JWT';
 };
 
+export type JWTCreateOptions<Header extends Record<string, any> = any, Body extends Record<string, any> = any> = {
+
+    readonly header: Header;
+    readonly body: Body;
+};
+
+export type JWTFixedCreateOptions<Header extends Record<string, any> = any, Body extends Record<string, any> = any> = {
+} & JWTCreateOptions<Header, Body>;
+
 export type JWTJoinedHeader<Header extends Record<string, any>> = Header & JWTFixedHeader;
 
 export type TokenTuple = [string, string, string];
